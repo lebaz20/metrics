fc90bea1dfcd3a9632b817461164d5db85a487e3
Mohamed Labib <mohamed.labib@vodafone.com>
2023-11-27 04:16:30 +0200

    :bug: Zip path does not exist

--- .github/workflows/uploadLambda.sh
+++ .github/workflows/uploadLambda.sh
@@ -9,6 +9,12 @@ if aws s3 ls "$S3_PREFIX" 2>&1 | grep -q 'NoSuchBucket'; then
   aws s3 mb "$S3_PREFIX"
 fi
 
+cd ../../src/api/
+npm install -g yarn
+yarn install
+yarn build  # Adjust this command based on your project
+
 # Copy files to the bucket
 cd ../../dist/api
+zip -r lambda-code.zip api/index.js
 aws s3 cp lambda-code.zip s3://$LAMBDA_BUCKET_NAME/lambda-code.zip
