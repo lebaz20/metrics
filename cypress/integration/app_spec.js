b15a82321e461670f1b33e5f78faf968b8379072
Mohamed Labib <mohamed.labib@vodafone.com>
2023-11-27 09:42:43 +0200

    :white_check_mark: Add E2E tests using Cypress

---
+++ cypress/integration/app_spec.js
@@ -0,0 +1,14 @@
+// cypress/integration/app_spec.js
+describe('App', () => {
+    it('renders app and submits form', () => {
+      cy.visit('http://localhost:3000'); // Assuming your app is running on this port
+  
+      // Find input fields and submit button
+      cy.findByLabelText(/Name/i).type('John');
+      cy.findByLabelText(/Value/i).type('42');
+      cy.findByText(/Submit/i).click();
+  
+      // Wait for the response to be displayed
+      cy.findByText(/Response:/i).should('exist');
+    });
+});
